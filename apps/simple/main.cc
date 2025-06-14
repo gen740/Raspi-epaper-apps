656e99fc90c604e6e49576580ca57692fce72c12
gen740 <54583542+gen740@users.noreply.github.com>
2025-06-14 18:57:56 +0900

    feat(simple): Expand buffer size

--- apps/simple/main.cc
+++ apps/simple/main.cc
@@ -1,8 +1,8 @@
 #include <chrono>
 #include <cstddef>
 #include <print>
-#include <vector>
 #include <thread>
+#include <vector>
 
 #include "DEV_Config.h"
 #include "EPD_7in3e.h"
@@ -19,7 +19,7 @@ class Epaper {
     EPD_7IN3E_Init();
     EPD_7IN3E_Clear(EPD_7IN3E_WHITE);  // WHITE
     DEV_Delay_ms(1000);
-    image_cache_.resize(static_cast<std::size_t>(480 * 800));
+    image_cache_.resize(static_cast<std::size_t>(4 * 480 * 800));
   }
 
   ~Epaper() {
@@ -55,7 +55,7 @@ auto main() -> int {
     std::println("Simple test Start!!");
     auto &epaper = Epaper::get();
     epaper.test();
-    std::this_thread::sleep_for(std::chrono::seconds(10));
+    std::this_thread::sleep_for(std::chrono::seconds(3));
   } catch (const std::exception &e) {
     std::println("Exception occur: {}", e.what());
   }
